86d447536aacc6600bf20fea7e37ec3e1908a528
Tycho Bokdam <tycho@palmtreecoding.com>
2020-05-18 15:08:47 +0200

    test: Added some tests

--- app/mobile/screens/Settings/About/__tests__/About.test.js
+++ app/mobile/screens/Settings/About/__tests__/About.test.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { shallow } from 'enzyme'
 import Updater  from 'update-react-native-app'
 
-import About from '../About'
+import { About } from '../About'
 
 jest.mock('update-react-native-app')
 
