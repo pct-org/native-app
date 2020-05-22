998aacdad62550eceff944b514eeca4eea72f675
Tycho Bokdam <tycho@palmtreecoding.com>
2020-05-22 10:43:47 +0200

    refactor: Remove everything TV related

--- app/components/BaseButton/index.js
+++ app/components/BaseButton/index.js
@@ -1,6 +1,1 @@
-import useCorrect from 'modules/useCorrect'
-
-import BaseButton from './BaseButton'
-import BaseButtonTV from './BaseButton.tv'
-
-export default useCorrect(BaseButton, null, BaseButtonTV)
+export { default } from './BaseButton'
--- app/components/Card/index.js
+++ app/components/Card/index.js
@@ -1,6 +1,1 @@
-import useCorrect from 'modules/useCorrect'
-
-import Card from './Card'
-import CardTV from './Card.tv'
-
-export default useCorrect(Card, null, CardTV)
+export { default } from './Card'
