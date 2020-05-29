c6f5a3d243cbf2f1dcb611759a80f9e9c421f4b8
Tycho Bokdam <tycho@palmtreecoding.com>
2020-05-29 17:53:00 +0200

    chore: Fix linting

--- app/modules/useAsyncStorage.js
+++ app/modules/useAsyncStorage.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import AsyncStorage from '@react-native-community/async-storage'
 
 export const useAsyncStorage = (key) => {
