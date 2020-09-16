d14bb54ab9be9fe926843cf7ebfe19f41cf22855
Tycho Bokdam <tycho@palmtreecoding.com>
2020-09-16 18:54:07 +0200

    refactor: Re-enable chromecast

--- android/app/src/main/java/com/popcorn/MainActivity.java
+++ android/app/src/main/java/com/popcorn/MainActivity.java
@@ -2,23 +2,22 @@ package com.popcorn;
 
 import android.os.Bundle;
 
+import com.facebook.react.GoogleCastActivity;
 
 import android.content.Intent;
 import android.content.res.Configuration;
 
 import org.devio.rn.splashscreen.SplashScreen;
 
-import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
 import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 
-public class MainActivity extends ReactActivity {
+public class MainActivity extends GoogleCastActivity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     SplashScreen.show(this, R.style.SplashTheme);
-
     super.onCreate(savedInstanceState);
   }
 
@@ -44,13 +43,10 @@ public class MainActivity extends ReactActivity {
   @Override
   protected ReactActivityDelegate createReactActivityDelegate() {
     return new ReactActivityDelegate(this, getMainComponentName()) {
-
       @Override
       protected ReactRootView createRootView() {
         return new RNGestureHandlerEnabledRootView(MainActivity.this);
       }
-
     };
   }
-
 }
