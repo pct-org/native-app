03b4aca515534c17b13e588f9a7fb64d1df915b7
Tycho Bokdam <tycho@palmtreecoding.com>
2020-10-01 17:47:46 +0200

    refactor: Enable check for updates again

--- app/mobile/root.js
+++ app/mobile/root.js
@@ -31,7 +31,7 @@ export default () => (
 
                 </BottomSheetManager>
 
-                {/*<CheckForUpdates />*/}
+                <CheckForUpdates />
 
               </WatchOnTvManager>
             </DownloadManager>
